9037a235225cc38041899a2a199eca60d72d3abf
oussama <oussama.ennadafy@gmail.com>
2022-06-17 10:44:50 +0100

    update

--- view/showProducts.php
+++ view/showProducts.php
@@ -76,7 +76,7 @@
     <!-- SECTION-CONTENT -->
     <section class="bg-gray-100 py-12">
     <div class="container max-w-screen-xl mx-auto px-4">
-      <h2 class="text-3xl font-bold mb-8">New products</h2>
+      <h2 class="text-3xl font-bold mb-8">All Products</h2>
 
       <div
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
@@ -85,7 +85,7 @@
         <div>
           <!-- COMPONENT: PRODUCT CARD -->
           <article class="shadow-sm rounded bg-white border border-gray-200 h-full shadow-sm">
-            <a href="http://localhost/fill-rouge/user/details/<?php echo $Product['id'] ?>" class="block relative p-1">
+            <a class="block relative p-1">
               <img
                 src="<?php echo '../../../fill-rouge/view/uploads/'.$Product['first_img']; ?>"
                 class="mx-auto w-auto"
@@ -95,9 +95,7 @@
             </a>
             <div class="p-4 border-t border-t-gray-200">
               <p class="font-semibold">$<?php echo $Product['price_item'] ?></p>
-              <a
-                href="http://localhost/fill-rouge/user/details/<?php echo $Product['id'] ?>"
-                class="block text-gray-600 mb-3 hover:text-blue-500"
+              <a class="block text-gray-600 mb-3"
               >
               <?php echo $Product['name_item'] ?>
               </a>
